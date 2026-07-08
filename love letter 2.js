8452265beea907143574bdc6491862fbb86769d5
Renan Souza <renanzinsouzas@gmail.com>
2026-07-08 13:01:33 -0300

    altera a senha

--- love letter 2.js
+++ love letter 2.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   var envelope = $("#envelope");
   var senhaInput = $("#senha");
-  var SENHA_CORRETA = "milena";
+  var SENHA_CORRETA = "mimi";
   var desbloqueado = false;
 
   function senhaValida() {
