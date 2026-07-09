f5a8fc925d787517f1c802f3c28def1a449bdde7
Renan Souza <renanzinsouzas@gmail.com>
2026-07-09 07:50:14 -0300

    Troca da senha e tittle

--- love letter 2.js
+++ love letter 2.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   var envelope = $("#envelope");
   var senhaInput = $("#senha");
-  var SENHA_CORRETA = "mimi";
+  var SENHA_CORRETA = "nina";
   var desbloqueado = false;
 
   function senhaValida() {
