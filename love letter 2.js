62b058b282ded9df3eae52a17ed39405a314de53
Renan Souza <renanzinsouzas@gmail.com>
2026-07-13 09:07:06 -0300

    Troca nome da palvra chave

--- love letter 2.js
+++ love letter 2.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   var envelope = $("#envelope");
   var senhaInput = $("#senha");
-  var SENHA_CORRETA = "nenem";
+  var SENHA_CORRETA = "monster";
   var desbloqueado = false;
 
   function senhaValida() {
